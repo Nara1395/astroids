77fd3d175d1501963517bd8e6500c97856b4538b
Nara1395 <nara.1395@gmail.com>
2025-07-15 10:23:27 +0800

    formatting code and added FPS counter

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import pygame
-import constants
 from constants import *
 
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
-        screen.fill(color="black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill(color="black")
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
