18e5539726c4bd46ee8d37960c1fdae578a2a7da
Nara1395 <nara.1395@gmail.com>
2025-07-18 16:08:14 +0800

    Modifying Player Class

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
     clock = pygame.time.Clock()
     dt = 0
 
@@ -13,8 +15,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
+
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
